80670238aa104ce01cfaf424d8e78391412cd6fd
siamr <siam_1000@hotmail.com>
2023-01-13 17:22:32 -0500

    about me page added

---
+++ pages/AboutMePage.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import selfie from '../public/cartoon-img.jpeg';
+import Image from 'next/image';
+
+function AboutMePage() {
+    return (
+        <div className="w-full h-screen bg-gradient-to-r from-gray-900 to-sky-900 p-4 text-white bg-white">
+            <div className="md:flex-row md:text-left text-center flex flex-col md:justify-evenly justify-center">
+                <div className="lg:max-w-3xl md:max-w-xl md:mt-28 flex flex-col">
+                    <h1 className="text-4xl bg-gradient-to-r from-slate-700 to-gray-200 bg-clip-text text-transparent">More About Me</h1>
+                    <p className="mt-2 text-2xl text-gray-300">Software Engineer</p>
+
+                    <p className="mt-5 text-lg text-gray-300"> 
+                        Hey, my name is Faiazur! I am a driven and skilled Software Engineer with 1 year of professional 
+                        experience in full-stack development. I specialize in creating scalable, responsive applications and have a proven track record of delivering 
+                        high-quality software on time and within budget. My expertise in multiple programming languages and development frameworks make me a valuable 
+                        asset to any team. With a passion for technology and a desire to continuously learn and improve, I am dedicated to building innovative and 
+                        user-friendly software that exceeds expectations.
+                    </p>
+
+                    <p className="mt-5 text-lg text-gray-300"> 
+                        I am a recent university graduate with a major in Applied Mathematics and Computer Science. I have a strong foundation in mathematical concepts and algorithms, 
+                        as well as experience in programming languages such as Python, C++ and Java. Throughout my studies, I have developed 
+                        problem-solving and analytical skills that I believe will make me a valuable addition to any team. 
+                        I am excited to apply the knowledge and skills I have acquired in university to real-world projects and to continue learning and growing as a 
+                        professional in the field of computer science.
+                    </p>
+
+                    <div className="md:justify-start flex justify-center">
+                        <div className="flex max-w-[80px]">
+                            <a href="/" className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Back</a>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+                <div className="md:mt-28 lg:mt-[210px] flex justify-center mt-5">
+                    <Image className="object-fill w-96 h-80 rounded-3xl" src={selfie}/>
+                </div>
+                
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default AboutMePage
--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,12 @@ export default function Home() {
               </li>
               <li>
                 <a href="/Contact" className="md:text-2xl text-xs bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 transition ease-in-out delay-100 bg-blue-500 hover:scale-125">
-                  Contact Me!
+                  Get in Touch!
+                </a>
+              </li>
+              <li>
+                <a href="/AboutMePage" className="md:text-2xl text-xs bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 transition ease-in-out delay-100 bg-blue-500 hover:scale-125">
+                  About Me
                 </a>
               </li>
             </ul>
